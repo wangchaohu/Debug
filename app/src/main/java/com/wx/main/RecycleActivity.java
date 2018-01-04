c83bd9142665e304e7b028ab01a92be1e5cdb69d
wangchaohu <xwangch@yeah.net>
2018-01-04 16:50:58 +0800

    悬浮按钮动画

--- app/src/main/java/com/wx/main/RecycleActivity.java
+++ app/src/main/java/com/wx/main/RecycleActivity.java
@@ -1,5 +1,8 @@
 package com.wx.main;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -11,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.wx.base.BaseActivity;
 import com.wx.customlottie.CustomLottieActivity;
@@ -27,6 +32,8 @@ import com.wx.write.WriteActivity;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.wx.utils.UtilsKt.checkVersion;
+
 
 /**
  * Created by xwangch on 16/8/2.
@@ -35,7 +42,7 @@ import java.util.List;
  * <p>
  * 可以根据数组长度的大小自动添加按钮，实现跳转
  */
-public class RecycleActivity extends BaseActivity {
+public class RecycleActivity extends BaseActivity implements View.OnClickListener{
 
     private List<String> chwang_s = Arrays.asList("Lottie简单使用", "Lottie自定义动画", "ViewDrag使用", "读写文件", "发送邮件", "日期时间选择", "自定义view",
             "生成二维码");
@@ -45,13 +52,48 @@ public class RecycleActivity extends BaseActivity {
     private SoundPool soundPool;
     private int soundId;  //创建某个声音对应的音频id
 
+    private RecyclerView recyclerView;
+    private ImageButton hoverButton, hoverButton1, hoverButton2 ,hoverButton3, hoverButton4, hoverButton5;
+    private int radius = 300;  //半径
+    private int count = 5;  //总数
+
     @Override
     public void initViews(Bundle savedInstanceState) {
         setContentView(R.layout.activity_recycleview_main);
         initSound();
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MyRecycleViewAdapter(this));
+        init();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initData();
+    }
+
+    private void init(){
+        recyclerView = (RecyclerView) findViewById(R.id.recycle);
+
+        hoverButton = (ImageButton) findViewById(R.id.hoverButton);
+        hoverButton1 = (ImageButton) findViewById(R.id.hoverButton1);
+        hoverButton2 = (ImageButton) findViewById(R.id.hoverButton2);
+        hoverButton3 = (ImageButton) findViewById(R.id.hoverButton3);
+        hoverButton4 = (ImageButton) findViewById(R.id.hoverButton4);
+        hoverButton5 = (ImageButton) findViewById(R.id.hoverButton5);
+
+        hoverButton.setOnClickListener(this);
+        hoverButton1.setOnClickListener(this);
+        hoverButton2.setOnClickListener(this);
+        hoverButton3.setOnClickListener(this);
+        hoverButton4.setOnClickListener(this);
+        hoverButton5.setOnClickListener(this);
+    }
+
+    private void initData(){
+        if (null != recyclerView) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new MyRecycleViewAdapter(this));
+        }
     }
 
     class MyRecycleViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
@@ -109,7 +151,7 @@ public class RecycleActivity extends BaseActivity {
      */
 
     private void initSound() {
-        if (UtilsKt.checkVersion(Build.VERSION_CODES.LOLLIPOP)){
+        if (checkVersion(Build.VERSION_CODES.LOLLIPOP)){
             soundPool = new SoundPool.Builder().build();
         }else {
             soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
@@ -126,5 +168,134 @@ public class RecycleActivity extends BaseActivity {
                 0,   //循环模式，0表示循环一次，-1表示一直循环，，+1表示当前循环次数
                 1   //播放速度，1是正常，范围0-2
         );
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.hoverButton:
+                openMenuAnimator();
+                break;
+            case R.id.hoverButton1:
+                UtilsKt.toast(this, "点击了hoverButton1", Toast.LENGTH_SHORT);
+                break;
+            case R.id.hoverButton2:
+                UtilsKt.toast(this, "点击了hoverButton2", Toast.LENGTH_SHORT);
+                break;
+            case R.id.hoverButton3:
+                UtilsKt.toast(this, "点击了hoverButton3", Toast.LENGTH_SHORT);
+                break;
+            case R.id.hoverButton4:
+                UtilsKt.toast(this, "点击了hoverButton4", Toast.LENGTH_SHORT);
+                break;
+            case R.id.hoverButton5:
+                closeMenuAnimator();
+                break;
+        }
+    }
+
+    /**
+     * 菜单按钮打开
+     * */
+
+    private void openMenuAnimator(){
+
+//       按钮1动画
+        openAnimator(hoverButton1, 0);
+//       按钮2动画
+        openAnimator(hoverButton2, 1);
+//       按钮3动画
+        openAnimator(hoverButton3, 2);
+//       按钮4动画
+        openAnimator(hoverButton4, 3);
+//       按钮5动画
+        openAnimator(hoverButton5, 4);
+
+    }
+    /**
+     * 菜单按钮关闭
+     * */
+
+    private void closeMenuAnimator(){
+        closeAnimator(hoverButton1, 0);
+        closeAnimator(hoverButton2, 1);
+        closeAnimator(hoverButton3, 2);
+        closeAnimator(hoverButton4, 3);
+        closeAnimator(hoverButton5, 4);
+    }
+
+    /**
+     * 打开动画
+     * */
+    private void openAnimator(View view, int index){
+
+        if (view.getVisibility() != View.VISIBLE){
+            view.setVisibility(View.VISIBLE);
+        }
+
+        double angle = Math.toRadians(90)/(count-1) * (index);
+        float x = -(float) ( Math.sin(angle) * radius);
+        float y = -(float) Math.cos(angle) * radius;
+
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(
+                ObjectAnimator.ofFloat(view, "translationX", 0, x),
+                ObjectAnimator.ofFloat(view, "translationY", 0, y),
+                ObjectAnimator.ofFloat(view, "alpha", 0, 1f),
+                ObjectAnimator.ofFloat(view, "scaleX", 0, 1f),
+                ObjectAnimator.ofFloat(view, "scaleY", 0, 1f)
+        );
+        animatorSet.setDuration(500).start();
+    }
+
+    /**
+     * 关闭动画
+     * */
+    private void closeAnimator(final View view, int index){
+        if (view.getVisibility() != View.VISIBLE){
+            view.setVisibility(View.VISIBLE);
+        }
+
+        double angle = Math.PI/2 /(count-1) * index;
+        float x = -(float) ( Math.sin(angle) * radius);
+        float y = -(float) Math.cos(angle) * radius;
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(
+                ObjectAnimator.ofFloat(view, "translationX", x, 0f),
+                ObjectAnimator.ofFloat(view, "translationY", y, 0f),
+                ObjectAnimator.ofFloat(view, "alpha", 1f, 0f),
+                ObjectAnimator.ofFloat(view, "scaleX", 1, 0.1f),  //当缩小为0时，不会实际改变控件位置
+                ObjectAnimator.ofFloat(view, "scaleY", 1, 0.1f)  //改为0.1这样防止关闭之后，原有位置还响应(方法一)
+        );
+
+        animatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+//                方案二，或将控件放大
+                if (view.getVisibility() != View.GONE){
+                    view.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        animatorSet.setDuration(500).start();
+
     }
 }
