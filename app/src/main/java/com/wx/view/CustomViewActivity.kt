b96b3e9ef0d166e95bfe82f3329fbeeaf2eb6c3a
wangchaohu <xwangch@yeah.net>
2018-01-05 17:35:42 +0800

    水波纹动画和屏幕写字

--- app/src/main/java/com/wx/view/CustomViewActivity.kt
+++ app/src/main/java/com/wx/view/CustomViewActivity.kt
@@ -3,6 +3,7 @@ package com.wx.view
 import android.os.Bundle
 import com.wx.base.BaseActivity
 import com.wx.debug.R
+import kotlinx.android.synthetic.main.activity_customview.*
 
 /**
  * Created by wx on 2017/12/29.
@@ -12,5 +13,11 @@ class CustomViewActivity : BaseActivity{
 
     override fun initViews(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_customview)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        myView.startAnimator()
     }
 }
